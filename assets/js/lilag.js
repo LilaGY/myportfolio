a8bbf3ac54a0ada5fbdde952886d0fbf80663ddb
LilaGY <lila.gueury@gmail.com>
2018-03-31 19:42:29 +0200

    update mail

---
+++ assets/js/lilag.js
@@ -0,0 +1,43 @@
+
+
+function sendMail() {
+
+    // get fields values to check if not empty
+
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    if (name.length && email.length && message.length) {
+
+        $.post('send.php', $('#mailform').serialize())
+
+        openModal();
+
+    }
+
+}
+
+
+function openModal() {
+    
+        // Get the modal
+        var modal = document.getElementById('myModal');
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+        
+        modal.style.display = "block";
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+}
