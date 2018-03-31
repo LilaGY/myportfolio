4034263be9f6bcf05e13d679120f777ed735f52e
LilaGY <lila.gueury@gmail.com>
2018-03-31 18:47:01 +0200

    first commit

---
+++ send.php
@@ -0,0 +1,36 @@
+<?php
+
+    use PHPMailer\PHPMailer\PHPMailer;
+
+    require 'PHPMailer.php';
+    require 'SMTP.php';
+    include 'credentials.php';
+
+    $mail             = new PHPMailer();
+
+    $mail->IsSMTP(); // telling the class to use SMTP
+    $mail->SMTPDebug  = 2;                     // enables SMTP debug information (for testing)
+                                            // 1 = errors and messages
+                                            // 2 = messages only
+    $mail->SMTPAuth   = true;                  // enable SMTP authentication
+    $mail->SMTPSecure = "tls";                 
+    $mail->Host       = "smtp.gmail.com";      // SMTP server
+    $mail->Port       = 587;                   // SMTP port
+    $mail->Username   = $username;  // username
+    $mail->Password   = $password;            // password
+
+    $mail->SetFrom(_REQUEST['email'], _REQUEST['name']);
+
+    $mail->Subject    = "Nouveau message de " . _REQUEST['name'];
+
+    $mail->MsgHTML(_REQUEST['message']);
+
+    $address = "lila.grv@hotmail.fr";
+    $mail->AddAddress($address, "Lila");
+
+    if(!$mail->Send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+    } else {
+    echo "Message sent!";
+    }
+?>
